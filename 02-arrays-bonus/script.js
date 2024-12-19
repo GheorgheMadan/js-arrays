5eb6f1fd59378b3bfc3a2c0174ffadc1c2f4051e
Gheorghe <madangheorghe.30@gmail.com>
2024-12-19 22:09:42 +0100

    complete bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,8 +15,8 @@ console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-  // const longNames = teachers.filter(teachers.length >= 5);
-  // console.log(longNames)
+const longNames = teachers.filter(name => name.length >=5);
+console.log(longNames)
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
@@ -25,8 +25,9 @@ delete teachers[1]
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 
-const isFabioPresent = teachers.indexOf(4);
+const isFabioPresent = teachers.includes("Fabio");
 console.log(isFabioPresent)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join(", ");
+console.log(teachersString)
